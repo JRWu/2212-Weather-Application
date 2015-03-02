c35f75559e11a831c9be0ed128fa2c7417100ce1
Jia Wu <jia_wu@hotmail.ca>
2015-03-01 20:21:16 -0500

    Adding basic menu layout. No actionlisteners implemented yet.

--- src/UpdateGUI.java
+++ src/UpdateGUI.java
@@ -1,4 +1,21 @@
+/**
+ *
+ * CS2212 6_TheWeather  
+ * UpdateGUI.java
+ * Usage: Handle updating and action events as mandated by the GUI.
+ * 
+ * @author Team6 
+ * @version 1.0 
+ */
+
+
 
 public class UpdateGUI {
-
+	
+	// Attributes
+	
+	//Constructor
+	
+	//Methods
+	
 }
--- src/main.java
+++ src/main.java
@@ -1,7 +1,36 @@
-public class main {
+/**
+ *
+ * CS2212 6_TheWeather  
+ * main.java
+ * Usage: Main program for 6_TheWeather.
+ * 
+ * @author Team6 
+ * @version 1.0 
+ */
 
+// Import Libraries
+import javax.swing.SwingUtilities;
+
+public class main {
     public static void main(String[] args) {
         System.out.println("Hello, Team6 WeatherJunkies");
+        
+        SwingUtilities.invokeLater(new Runnable(){
+//        	@Override
+        	public void run(){
+        		DesignWindow window = new DesignWindow();
+        		window.setVisible(true);
+        	}
+        });
+        
+        
     }
+    
+    
+    
+    
+    
+    
+    
 
 }
