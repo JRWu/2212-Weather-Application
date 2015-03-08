15d829226f5f64087cef3466114c4d04d4b34142
Anthony Bushara <abushara@uwo.ca>
2015-03-08 15:13:41 -0400

    Added Javadoc and cleaned some things up

--- src/JSON.java
+++ src/JSON.java
@@ -27,11 +27,16 @@ public class JSON {
 	private String location = ""; //String to contain location
 	private URL url;	//URL Object, used to create connection
 	private double temp,temp_max,temp_min,windSpeed; //all the variables for CURRENT weather (forecast still needs to be figured out)
-	private int pressure, humidity, windDirectionDegree,time; 
+	private int pressure, humidity, windDirectionDegree,time,sunrise,sunset; 
 	private String weatherDescription, skyState, windDirection; //This may change, need to test out how the currentWeatherSetVariables function is for a while
 	private JSONObject allWeatherData; 
 	
 	
+	/**
+	 * Returns a JSON object to use for obtaining weather data. 
+	 * 
+	 * @param l the string for the location JSON object will be grabbing. 
+	 */
 	public JSON (String l){ //When creating the JSON object, initially set the location (eg, London, CA for our city)
 		
 		location = l;
@@ -40,18 +45,16 @@ public class JSON {
 		
 	}
 	
-	public JSONObject requestJson(String time){ //was in the UML diagram, will most likely be axed out for different implementation
-		
-		
-		return allWeatherData;
 	
 	
-	}
-	
+	/**
+	 * Method to obtain current weather data; in the future it will throw an error if it cannot connect. 
+	 * 
+	 * @return a Current object with all fields filled for the current weather data. 
+	 */
 	public Current updateCurrentWeatherData(){ //
 		try{
 			url = new URL(HOST + PATH_CURRENT + query);
-			System.out.println(HOST + PATH_CURRENT + query);
 			HttpURLConnection connect = (HttpURLConnection) url.openConnection();
 			InputStream in = connect.getInputStream();
 			String jsonString = IOUtils.toString(in);
@@ -62,41 +65,51 @@ public class JSON {
 			JSONObject main = currentWeatherData.getJSONObject("main");
 			JSONArray weather = currentWeatherData.getJSONArray("weather");
 			JSONObject wind = currentWeatherData.getJSONObject("wind");
+			JSONObject sun = currentWeatherData.getJSONObject("sys");
 			currentMainSetVariables(main);
 			currentWeatherSetVariables(weather);
 			currentWindSetVariables(wind);
+			currentSunTime(sun);
 			//return ADO_Object
-			return new Current(5,pressure,windSpeed,temp,temp_min,temp_max,humidity,windDirection,skyState);
+			return new Current(time,pressure,windSpeed,temp,temp_min,temp_max,humidity,windDirection,skyState);
 			
 		}catch (IOException e){
 			System.out.println(e.getMessage());
 		}
 		return null;
 	}
+
 	
-	public static void main (String [] args){
-		System.out.println("Creating JSON");
-		JSON asdf = new JSON("London,CA");
-		String qwer = "hello";
-		System.out.println(qwer);
-		
-	}
-	
+	/**
+	 * Private method to set all temperature and humidity and pressure fields
+	 * 
+	 * @param main JSONObject containing these fields.
+	 */
 	private void currentMainSetVariables(JSONObject main){
 		humidity = main.getInt("humidity");
 		pressure = main.getInt("pressure");
 		temp_max = main.getDouble("temp_max");
 		temp_min = main.getDouble("temp_min");
 		temp = main.getDouble("temp");
-	}
+		}
 	
+	/**
+	 * Private method for weather variables. Gets sky state and weather description
+	 * 
+	 * @param weather JSONArray containing all weather fields
+	 */
 	private void currentWeatherSetVariables(JSONArray weather){
-		JSONObject fdsa = weather.getJSONObject(0);//easier to represent the current weather as a JSONObject than array
-		weatherDescription = fdsa.getString("description");
-		skyState = fdsa.getString("main");
+		JSONObject weatherData= weather.getJSONObject(0);//easier to represent the current weather as a JSONObject than array
+		weatherDescription = weatherData.getString("description");
+		skyState = weatherData.getString("main");
 		
 	}
 	
+	/**
+	 * Private method that gets wind data and converts the degree to a rough direction
+	 * 
+	 * @param wind JSONObject containing all wind data
+	 */
 	private void currentWindSetVariables(JSONObject wind){
 		windDirectionDegree = wind.getInt("deg");
 		windSpeed = wind.getDouble("speed");
@@ -108,6 +121,16 @@ public class JSON {
 			windDirection = "South";
 		else
 			windDirection = "West";
+	}
+	
+	/**
+	 * Private method that gets data for sunrise and sunset and sets it
+	 * 
+	 * @param sun JSONObject for sunrise and sunset data
+	 */
+	private void currentSunTime(JSONObject sun){
+		sunrise = sun.getInt("sunrise");
+		sunset = sun.getInt("sunset");
 	}
 	
 	
