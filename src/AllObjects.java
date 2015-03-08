9df1c8ee5874950776128df4eb9e290e809a3693
Bill Buis <wbuis@uwo.ca>
2015-03-08 14:53:29 -0400

    some added stuff

--- src/AllObjects.java
+++ src/AllObjects.java
@@ -9,25 +9,39 @@ public class AllObjects extends ADO implements java.io.Serializable {
 /**
 * Instance variables 
 */
-    private Daily[] longTerm;     
-    private Hourly[] shortTerm;   
-    private Current current;
+	private Daily[] longTerm;     
+	private Hourly[] shortTerm;   
+    	private Current current;
+/**
+* Constructor for AllObjects that will take JSON objects as paramaters and create objects for the long term, short term and current forecast from that
+*/
+	public AllObjects(){
+        	current = new Current(0, 0, 0, 0, 0, 0, 0, null,null);
+        	shortTerm = new Hourly[8];
+        	for (int i=0; i<8; i++){
+            		shortTerm[i] = null;
+        	}
+        	longTerm = new Daily[5];
+        	for (int i=0; i<5; i++){
+            		longTerm[i] = null;
+        	}
+    	}
 /**
 * @return an array of type Daily to represent the long term forecast
 */
-    public Daily[] getLongTerm(){
-        return longTerm;
-    }
+    	public Daily[] getLongTerm(){
+   		return longTerm;
+    	}
 /**
 * @return an array of type Hourly to represent the short term forecast
 */
-    public Hourly[] getShoterTerm(){
-        return shortTerm;
-    }
+	public Hourly[] getShortTerm(){
+        	return shortTerm;
+    	}
 /**
 * @return an object of type Current to represent the current forecast
 */    
-    public Current getCurrent(){
-        return current;
-    }    
+	public Current getCurrent(){
+        	return current;
+    	}    
 }
