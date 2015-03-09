3f9805dd6d4d3fb6b48f89cbd2be94d651112180
Bill Buis <wbuis@uwo.ca>
2015-03-08 22:48:36 -0400

    Updating Javadocs

--- src/ADO.java
+++ src/ADO.java
@@ -1,33 +1,48 @@
 import java.io.*;
+import javax.swing.ImageIcon;
+import org.apache.commons.io.*;
+
 /**
-* This class represents the general instances variables and classes shared between the different objects
+* March 8, 2015
+* CS 2212
 * @author Team 6
+* This class represents the general instances variables and classes shared between the different objects
 */
 public class ADO implements java.io.Serializable
 {
+
 /**
 * Instance variables
 */
 	private int airPressure;
 	private double windSpeed;
-	private double temperature;
+	protected double temperature;
 	private double minTemp;
 	private double maxTemp;
 	private int humidity;
 	private String windDirection;
 	private String skyCondition;
-	private String userPreferences;
+	private String userPreferences = "";
+	private ImageIcon skyState;
 /**
-* General constructor
+* This serves as a general constructor for the abstract data object with no parameters
 */
 	public ADO(){};
 /*
-* Constructor for the abstract data object
+* Constructor for the abstract data object that initializes instance variables and loads the user preferences for units from a text file
+* @param air the air pressure for the abstract data object
+* @param wind the wind speed for the abstract data object
+* @param temp the current temperature for the abstract data object
+* @param min the minimum temperature for the abstract data object
+* @param max the maximum temperature for the abstract data object
+* @param humid the humidity for the abstract data object
+* @param windDir the wind direction for the abstract data object
+* @param sky the sky condition for the abstract data object
+* @param state the image that goes along with the associated sky condition
 */
-	public ADO (String user, int air, double wind, double temp, double min, double max,int humid, String windDir, String sky)
+	public ADO (int air, double wind, double temp, double min, double max,int humid, String windDir, String sky, ImageIcon state)
 	{
-		userPreferences = user;
-		serializePreferences(userPreferences);
+		deSerializePreferences();
 		airPressure = air;
 		windSpeed = wind;
 		temperature = convertTemp(temp,userPreferences);
@@ -36,133 +51,187 @@ public class ADO implements java.io.Serializable
 		humidity = humid;
 		windDirection = windDir;
 		skyCondition = sky;
+		skyState = state;
 	}
 /**
-* Getter and setter methods for the air pressure
+*
+* @return The air pressure of the abstract data object
 */
 	public int getAirPressure()
 	{
 		return airPressure;
 	}
+/**
+* This method sets the air pressure for the abstract data object
+* @param pressure the air pressure value to be set
+*/
 	public void setAirPressure(int pressure)
 	{
 		airPressure = pressure;
 	}
 /**
-* Getter and seter methods for the wind speed
+*
+* @return The wind speed of the abstract data object
 */
 	public double getWindSpeed()
 	{
 		return windSpeed;
 	}
+/**
+* This method sets the wind speed for the abstract data object
+* @param wind the wind speed value to be set
+*/
 	public void setWindSpeed(double wind)
 	{
 		windSpeed = wind;
 	}
 /**
-* Getter and setter methods for the temperature
+*
+* @return The current temperature of the abstract data object
 */
 	public double getTemperature()
 	{
 		return temperature;
 	}
+/**
+* This method sets the current temperature for the abstract data object
+* @param temp the temperature value to be set
+*/
 	public void setTemperature (double temp)
 	{
 		temperature = convertTemp(temp,userPreferences);
 	}
 /**
-* Getter and setter methods for the expected minimum temperature
+*
+* @return The minimum temperature for the abstract data object
 */
 	public double getMinTemp()
 	{
 		return minTemp;
 	}
+/**
+* This method sets the minimum temperature for the abstract data object
+* @param min the minimum temperature value to be set
+*/
 	public void setMinTemp(double min)
 	{
 		minTemp = convertTemp(min,userPreferences);
 	}
 /**
-* Getter and setter methods for the expected maximum temperature
+*
+* @return The max temperature of the abstract data object
 */
 	public double getMaxTemp()
 	{
 		return maxTemp;
 	}
+/**
+* This method sets the maximum temperature value for the abstract data object
+* @param max the maximum temperature value to be set
+*/
 	public void setMaxTemp(double max)
 	{
 		maxTemp = convertTemp(max,userPreferences);
 	}
 /**
-* Getter and setter methods for the humidity
+*
+* @return The humidity of the abstract data object
 */
 	public int getHumidity()
 	{
 		return humidity;
 	}
+/**
+* This method sets the humidity for the abstract data object
+* @param humid the humidity value to be set
+*/
 	public void setHumidity(int humid)
 	{
 		humidity = humid;
 	}
 /**
-* Getter and setter methods for the wind direction
+*
+* @return The wind direction of the abstract data object
 */
 	public String getWindDirection()
 	{
 		return windDirection;
 	}
+/**
+* This method sets the wind direction for the abstract data object
+* @param direction The wind direction to be set
+*/
 	public void setWindDirection(String direction)
 	{
 		windDirection = direction;
 	}
 /**
-* Getter and setter methods for the sky condition
+*
+* @return The condition of the sky for the abstract data object
 */
 	public String getSkyCondition()
 	{
 		return skyCondition;
 	} 
+/**
+* This method sets the sky condition for the abstract data object
+* @param condition The sky condition to be set 
+*/
 	public void setSkyCondition(String condition)
 	{
 		skyCondition = condition;
 	}
 
 /**
-* Getter and setter methods for user preferences
+*
+* @return The current user preferences for the units to be used for the temperature
 */
 	public String getPreferences()
 	{
 		return userPreferences;
-	}
-/**
-* Current user preferences are deserialized and new ones are serialized
-*/
-	public void setPreferences(String user)
-	{
-		deSerializePreferences(userPreferences);
-		userPreferences = user;
-		serializePreferences(userPreferences);
 	}	
 	
 /**
-* Converts the temperature value from kelvin to celsius, kelvin to fahrenheit or leaves it based on user preferences
+*
+* @return the image associated with the current sky state
+*/
+	public ImageIcon getCondition()
+	{
+		return skyState;
+	}
+/**
+*
+* @param newState the new image to represented the updated sky state
+*/
+	public void setCondition(ImageIcon newState)
+	{
+		skyState = newState;
+	}
+/**
+* This method converts the temperature value from kelvin to celsius, kelvin to fahrenheit or leaves it in kelvin based on user preferences
+* @param temp The temperature value to be converted 
+* @param userPreferences The user preferences that indicate which type of units the user would like for the temperature to be displayed with
 */
 
-	private double convertTemp(double temperature, String userPreferences)
+	private double convertTemp(double temp, String preferences)
 	{
-		if (userPreferences.equals("fahrenheit"))
+                double temperature;
+		if (preferences.equals("fahrenheit"))
 		{
-			temperature = ((temperature -273.15)*1.8) + 32;
+                    temperature = ((temp -273.15)*1.8) + 32;
 		}
-		if (userPreferences.equals("celsius"))
+                else if (preferences.equals("celsius"))
 		{
-			temperature = temperature - 273.15;
-		}
+                    temperature = temp - 273.15;
+		} else {
+                    temperature = temp;
+                }
 		return temperature;
 	}
 /**
 * This method saves the user preferences to an external text file
+* @param preferences The user preferences to be saved to an external text file
 */
-
 	public void serializePreferences (String preferences)
 	{
 		try 
@@ -175,25 +244,38 @@ public class ADO implements java.io.Serializable
 		{
 			e.printStackTrace();
 		}
+		userPreferences = preferences;
 	}
 
 /**
-* This method deserializes the current user preferences
+* This method deserializes the user preferences contained in the text file and loads them to the class
 */
-	public void deSerializePreferences(String preferences)
-	{
-		userPreferences = null;
-		try
+		public void deSerializePreferences()
+		{
+			userPreferences = "";
+			try
 		{
 			FileInputStream fin = new FileInputStream("preferences.ser");
 			ObjectInputStream in = new ObjectInputStream (fin);
 			userPreferences = (String)in.readObject();
+			if (userPreferences == null)
+				userPreferences ="";
 			in.close();
 			fin.close();
+		}catch (FileNotFoundException e)
+		{
+			serializePreferences(userPreferences);
 		}catch (Exception e)
 		{
 			e.printStackTrace();
 		}
 		
+	}
+	public static void main(String[]args)
+	{
+		ADO ad = new ADO();
+		ad.temperature = 100.0;
+		ad.temperature = ad.convertTemp(ad.temperature, "celsius");
+		System.out.println(ad.temperature);
 	}
 }
--- src/Hourly.java
+++ src/Hourly.java
@@ -1,22 +1,47 @@
 import java.io.*;
+import javax.swing.ImageIcon;
 /**
+* March 8, 2014
+* CS 2212
+* @author team 6
 * This class represents the object for weather in 3 hour intervals
 */
 public class Hourly extends ADO implements java.io.Serializable
 {
+/**
+* Instance variables
+*/
 	private int hour;
-	public Hourly(int time, String user, int air, double wind, double temp, double min, double max, int humid, String windDir, String sky)
+/**
+* Constructor for the hourly weather object
+* @para time the current hour for the weather object
+* @param air the air pressure for the weather object
+* @param wind the wind speed for the weather object
+* @param temp the current temperature for the weather object
+* @param min the minimum temperature for the weather object
+* @param max the maximum temperature for the weather object
+* @param humid the humidity for the weather object
+* @param windDir the wind direction for the weather object
+* @param sky the sky condition fo the weather object
+* @param state the image that goes along with the associated sky condition
+*/
+	public Hourly(int time, int air, double wind, double temp, double min, double max, int humid, String windDir, String sky, ImageIcon state)
 	{
-		super (user, air,wind,temp,min,max,humid,windDir,sky);
+		super (air,wind,temp,min,max,humid,windDir,sky, state);
 		hour = time;
 	}
 /**
-* Getter and setter methods for the current hour
+* 
+* @return the current hour that the object is representing
 */
 	public int getHour()
 	{
 		return hour;
 	}
+/**
+* This method sets the current hour that the weather object is representing
+* @param hourly the hour to be set
+*/
 	public void setHour(int hourly)
 	{
 		hour = hourly;
