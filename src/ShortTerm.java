67e5a57178c3e0ae10be8638a66005958af26c62
Bill Buis <wbuis@uwo.ca>
2015-03-15 18:00:54 -0400

    Long term and short term data classes added, allobjects is obsolete now so it has been removed

--- src/ShortTerm.java
+++ src/ShortTerm.java
@@ -13,46 +13,157 @@ public class ShortTerm extends ADO implements java.io.Serializable
 /**
 * Instance variables 
 */
-	private Daily[] shortTerm;
+	private Hourly[] shortTerm;
 /**
 * Constructor for the short term forecast
-* @day1 the object representing the monday forecast
-* @day2 the object representing the tuesday forecast
-* @day3 the object representing the wednesday forecast
-* @day4 the object representing the thursday forecast
-* @day5 the object representing the friday forecast
+* @param hours the array representing the array of hourly objects for the short term forecast
 */
-	public ShortTerm(Daily day1, Daily day2, Daily day3, Daily day4, Daily day5)
+	public ShortTerm(Hourly[] hours)
 	{
-		shortTerm = new Daily[5];
-		shortTerm[0] = day1;
-		shortTerm[1] = day2;
-		shortTerm[2] = day3;
-		shortTerm[3] = day4;
-		shortTerm[4] = day5;
+		shortTerm = hours;
 	}
 /**
 * Setter method for the short term forecast
-* @day1 the object representing the monday forecast
-* @day2 the object representing the tuesday forecast
-* @day3 the object representing the wednesday forecast
-* @day4 the object representing the thursday forecast
-* @day5 the object representing the friday forecast
+* @param hours the array representing the array of hourly objects for the short term forecast
 */
-	public void setShortTerm(Daily day1, Daily day2, Daily day3, Daily day4, Daily day5)
+	public void setShortTerm(Hourly[] hours)
 	{
-		shortTerm[0] = day1;
-		shortTerm[1] = day2;
-		shortTerm[2] = day3;
-		shortTerm[3] = day4;
-		shortTerm[4] = day5;
+		shortTerm = hours;
 	}
 /**
 *
 *@return the short term foreceast object
 */
-	public Daily[] getShortTerm()
+	public Hourly[] getShortTerm()
 	{
 		return shortTerm;
+	}
+/**
+*
+*@return the first 3 hours 
+*/
+	public Hourly getHourly1()
+	{
+		return shortTerm[0];
+	}
+/**
+* This method sets the first 3 hours of the short term forecast
+* @param hour the object representing the first 3 hours
+*/
+	public void setHourly1(Hourly hour)
+	{
+		shortTerm[0] = hour;
+	}
+/**
+*
+*@return the object for 3 - 6 am
+*/
+	public Hourly getHourly2()
+	{
+		return shortTerm[1];
+	}
+/**
+* This method sets the hourly object for 3-6 am
+* @param hour the object representing the forecast for 3-6 am
+*/
+	public void setHourly2(Hourly hour)
+	{
+		shortTerm[1] = hour;
+	}
+/**
+*
+*@return the object for 6 - 9 am
+*/
+	public Hourly getHourly3()
+	{
+		return shortTerm[2];
+	}
+/**
+* This method sets the hourly object for 6-9 am
+* @param hour the object representing the forecast for 6-9 am
+*/
+	public void setHourly3(Hourly hour)
+	{
+		shortTerm[2] = hour;
+	}
+/**
+*
+*@return the object for 9am - 12 pm
+*/
+	public Hourly getHourly4()
+	{
+		return shortTerm[3];
+	}
+/**
+* This method sets the hourly object for 9am -12pm
+* @param hour the object representing the forecast for 9 am -12 pm
+*/
+	public void setHourly4(Hourly hour)
+	{
+		shortTerm[3] = hour;
+	}
+/**
+*
+*@return the object for 12pm - 3 pm
+*/
+	public Hourly getHourly5()
+	{
+		return shortTerm[4];
+	}
+/**
+* This method sets the hourly object for 12 pm -3pm
+* @param hour the object representing the forecast for 12 pm -3 pm
+*/
+	public void setHourly5(Hourly hour)
+	{
+		shortTerm[4] = hour;
+	}
+/**
+*
+*@return the object for 3 pm - 6 pm
+*/
+	public Hourly getHourly6()
+	{
+		return shortTerm[5];
+	}
+/**
+* This method sets the hourly object for 3 pm - 6 pm
+* @param hour the object representing the forecast for 3 pm -6 pm
+*/
+	public void setHourly6(Hourly hour)
+	{
+		shortTerm[5] = hour;
+	}
+/**
+*
+*@return the object for 6 pm -9 pm
+*/
+	public Hourly getHourly7()
+	{
+		return shortTerm[6];
+	}
+/**
+* This method sets the hourly object for 6 pm - 9 pm
+* @param hour the object representing the forecast for 6 pm -9 pm
+*/
+	public void setHourly7(Hourly hour)
+	{
+		shortTerm[6] = hour;
+	}
+/**
+*
+*@return the object for 9pm - 12 am
+*/
+	public Hourly getHourly8()
+	{
+		return shortTerm[7];
+	}
+/**
+* This method sets the hourly object for 9pm -12am
+* @param hour the object representing the forecast for 9 pm -12 am
+*/
+	public void setHourly8(Hourly hour)
+	{
+		shortTerm[7] = hour;
 	}
 }
