dd6ef59be7d4795eb19b9ece9171625fba42af4d
Bill Buis <wbuis@uwo.ca>
2015-03-22 22:09:22 -0400

    Mars parameters fixed

--- src/main/java/Mars.java
+++ src/main/java/Mars.java
@@ -12,14 +12,12 @@ public class Mars extends ADO{
  * @param air the air pressure on mars
  * @param wind the wind speed on mars
  * @param temp the temperature on mars
- * @param min the expected minimum temperature on mars
- * @param max the expected maximum temperature on mars
  * @param humid the expected humidity on mars
  * @param windDir the wind direction on mars
  * @param sky the state of the sky on mars
  * @param state an image representing the sky state
  */
-	public Mars(int air, double wind, double temp, double min, double max,int humid, String windDir, String sky, ImageIcon state)
+	public Mars(int air, double wind, double temp, int humid, String windDir, String sky, ImageIcon state)
 	{
 		super(air,wind,temp,humid,windDir,sky,state);
 	}
